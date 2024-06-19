4b2ead74e8a4cbd4db8cc1f6ec3a6a9ab2a0caf5
Zhivko Vasilev <support@mailtemi.com>
2024-06-19 08:00:21 +0300

    cleanup

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -15,45 +15,6 @@ export type TIndexData = {
   render: () => Promise<void>;
 };
 
-export type TCareerIndex = {
-  id: string;
-  slug: string;
-  body: string;
-  collection: string;
-  data: {
-    title: string;
-    page_title: string;
-    image: string;
-    description?: string;
-    meta_title?: string;
-    benifits: {
-      title: string;
-      description: string;
-      benifit_list: {
-        title: string;
-        content: string;
-        color: string;
-        icon: string;
-      }[];
-    };
-    sidebar_content: {
-      title: string;
-      content: string;
-      button: {
-        label: string;
-        link: string;
-        enable: boolean;
-      };
-      enable: boolean;
-    };
-    career: {
-      title: string;
-      subtitle: string;
-    };
-  };
-  render: () => Promise<void>;
-};
-
 export type TIntegrationsIndex = {
   id: string;
   slug: string;
